c75090c872f4e31ee1d00786c1a86c228cc24347
nikita <79851214062@yandex.ru>
2021-10-28 21:37:29 +0300

    clear

--- src/components/atoms/GridContainer/GridContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react"
-import { Styled } from "./styled"
-
-export const GridContainer: FC = ({children}) => {
-    return (
-        <Styled.GridContainer>{children}</Styled.GridContainer>
-    )
-}
-
--- src/pages/Game.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Board } from "../components"
-
-export const Game = () => {
-    return (
-        <Board />
-    )
-}
