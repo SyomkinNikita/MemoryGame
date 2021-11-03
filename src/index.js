b2f10048996f70efd6b4d47eddf24e24f02a625b
nikita <79851214062@yandex.ru>
2021-11-03 23:02:29 +0300

    added webpack-dev-server and html-webpack-plugin

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import './styles.css';
+
+render(<div className="title">React app</div>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
-const buildPath = path.resolve(__dirname, 'dist')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const buildPath = path.resolve(__dirname, 'dist');
+const srcPath = path.resolve(__dirname, 'src');
 
 module.exports = {
-  entry: path.resolve(__dirname, './index.js'),
+  entry: path.resolve(__dirname, 'src'),
   output: {
     path: buildPath,
     filename: "bundle.js"
@@ -10,9 +12,22 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.jsx?/,
         use: 'babel-loader'
       }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(srcPath, 'index.html')
+    })
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: 9000
   }
 }
